be1190fad5119d313f0110c7dc71dbc7b5a7d45e
VitorOM2 <viktormarque2053@gmail.com>
2021-09-26 17:10:57 -0300

    Arquivo adiconado novamente com o arquivo .gitignore funcionando

---
+++ Calculadora/src/calculadora/Main.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Vitor
+ */
+public class Main {
+    
+    public static void main(String[] args)
+    {
+        Calculadora app = new Calculadora();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
